ae628ca2450b32e7374c373cd706b1daaadb31a0
IDK-Silver <60951976+IDK-Silver@users.noreply.github.com>
2024-09-16 11:03:20 +0800

    Update setup_submodule.py

--- setup_submodule.py
+++ setup_submodule.py
@@ -1,8 +1,7 @@
 import subprocess
 
 subprocess.run(["git", "submodule", "init"])
-
-subprocess.run(["git", "submodule", "init"])
+subprocess.run(["git", "submodule", "update", '--recursive'])
 subprocess.run(['pip', 'install', '-e', '.'], cwd='./src/real_esrgan_generator/extern/Real_ESRGAN/')
 with open('src/real_esrgan_generator/extern/Real_ESRGAN/__init__.py', 'w') as export:
     export.write("")
